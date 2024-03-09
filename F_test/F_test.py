ad380a9ef7823f795218e447f49b90e5d65e1b6d
xingchen-fan <xf82@cornell.edu>
2024-03-09 02:14:56 +0100

    Better

--- F_test/F_test.py
+++ F_test/F_test.py
@@ -15,41 +15,77 @@ if not(args.method == "Chi2" or args.method == "NLL") :
     sys.exit(1)
 
 
-def singleBernFTest(x, gauss_mu, histogram, cat = "", method = "Chi2", e_type = "Poisson", offset = False):
-    bern2_model = Bern2Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 10, 7., 105., \
-                                  printLevel = -1, eps = 1, offSet = offset, strategy = 0)
-    bern2_model[0].Print("v")
-    bern3_model = Bern3Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
-                                  printLevel = -1, eps = 1, offSet = offset, strategy = 0)
-    bern3_model[0].Print("v")
-    bern4_model = Bern4Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
-                                  printLevel = -1, eps = 1, offSet = offset, strategy = 0)
-    bern4_model[0].Print("v")
-    bern5_model = Bern5Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
-                                  printLevel = -1, eps = 1, offSet = offset, strategy = 0)
-    bern5_model[0].Print("v")
+# def singleBernFTest(x, gauss_mu, histogram, cat = "", method = "Chi2", e_type = "Poisson", offset = False):
+#     bern2_model = Bern2Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 10, 7., 105., \
+#                                   printLevel = -1, eps = 1, offSet = offset, strategy = 0)
+#     bern2_model[0].Print("v")
+#     bern3_model = Bern3Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
+#                                   printLevel = -1, eps = 1, offSet = offset, strategy = 0)
+#     bern3_model[0].Print("v")
+#     bern4_model = Bern4Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
+#                                   printLevel = -1, eps = 1, offSet = offset, strategy = 0)
+#     bern4_model[0].Print("v")
+#     bern5_model = Bern5Minization(x, gauss_mu, histogram, method, e_type, cat, 10, 0.3, 50, 7., 105.,\
+#                                   printLevel = -1, eps = 1, offSet = offset, strategy = 0)
+#     bern5_model[0].Print("v")
 
     
-    stat = [bern2_model[1], bern3_model[1], bern4_model[1], bern5_model[1]]
+#     stat = [bern2_model[1], bern3_model[1], bern4_model[1], bern5_model[1]]
+#     fs = []
+#     if method == "Chi2":
+#         for i in range(len(stat) - 1):
+#             fs.append(ROOT.Math.fdistribution_cdf_c((stat[i] - stat[i+1])*(260-5-i)/stat[i+1], 1, 260-5-i))
+#     elif method == "NLL":
+#         for i in range(len(stat) - 1):
+#             fs.append(ROOT.Math.chisquared_cdf_c(2*(stat[i] - stat[i+1]), 1))
+
+#     print(method, " = ", stat)
+#     print("P-value = ", fs)
+
+def singleBernFTest(x, gauss_mu, histogram, cat = "", method = "Chi2", e_type = "Poisson", eps = 0.1, offset = False, strategy = 0):
+    bern2_model = Bern2Class(x, gauss_mu, cat, 10, 0.3, 10, 7., 105.)
+    bern3_model = Bern3Class(x, gauss_mu, cat, 10, 0.3, 50, 7., 105.)
+    bern4_model = Bern4Class(x, gauss_mu, cat, 10, 0.3, 50, 7., 105.)
+    bern5_model = Bern5Class(x, gauss_mu, cat, 10, 0.3, 50, 7., 105.)
+    if e_type == "Poisson": error = ROOT.RooFit.DataError(ROOT.RooAbsData.Poisson)
+    elif e_type == "SumW2": error = ROOT.RooFit.DataError(ROOT.RooAbsData.SumW2)
+
+    if method == "Chi2": 
+        stat1 = ROOT.RooChi2Var("stat_bern2_" + cat, "stat bern2 " + cat, bern2_model.pdf,  histogram, error)
+        stat2 = ROOT.RooChi2Var("stat_bern3_" + cat, "stat bern3 " + cat, bern3_model.pdf,  histogram, error)
+        stat3 = ROOT.RooChi2Var("stat_bern4_" + cat, "stat bern4 " + cat, bern4_model.pdf,  histogram, error)
+        stat4 = ROOT.RooChi2Var("stat_bern5_" + cat, "stat bern5 " + cat, bern5_model.pdf,  histogram, error)
+    elif method == "NLL":
+        stat1 = ROOT.RooNLLVar("stat_bern2_" + cat, "stat bern2 " + cat, bern2_model.pdf,  histogram)
+        stat2 = ROOT.RooNLLVar("stat_bern3_" + cat, "stat bern3 " + cat, bern3_model.pdf,  histogram)
+        stat3 = ROOT.RooNLLVar("stat_bern4_" + cat, "stat bern4 " + cat, bern4_model.pdf,  histogram)
+        stat4 = ROOT.RooNLLVar("stat_bern5_" + cat, "stat bern5 " + cat, bern5_model.pdf,  histogram)
+
+    stats = [stat1, stat2, stat3, stat4]
+    if method == "Chi2": 
+        for entry in stats:
+            Minimizer_Chi2(entry, -1, 100, False, strategy)
+            Minimizer_Chi2(entry, -1, eps, offset, strategy)
+    elif method == "NLL": 
+        for entry in stats:
+            Minimizer_NLL(entry, -1, 100, False, strategy)
+            Minimizer_NLL(entry, -1, eps, offset, strategy)
+    output = [stat1.getVal(), stat2.getVal(), stat3.getVal(), stat4.getVal()]
     fs = []
     if method == "Chi2":
-        for i in range(len(stat) - 1):
-            fs.append(ROOT.Math.fdistribution_cdf_c((stat[i] - stat[i+1])*(260-5-i)/stat[i+1], 1, 260-5-i))
+        for i in range(len(output) - 1):
+            fs.append(ROOT.Math.fdistribution_cdf_c((output[i] - output[i+1])*(260-5-i)/output[i+1], 1, 260-5-i))
     elif method == "NLL":
-        for i in range(len(stat) - 1):
-            fs.append(ROOT.Math.chisquared_cdf_c(2*(stat[i] - stat[i+1]), 1))
+        for i in range(len(output) - 1):
+            fs.append(ROOT.Math.chisquared_cdf_c(2*(output[i] - output[i+1]), 1))
 
-    print(method, " = ", stat)
+    print(method, " = ", output)
     print("P-value = ", fs)
 
 
 
-
-
-
-
-ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.WARNING)
-# ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
+# ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.WARNING)
+ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
 lowx = 105.
 
 # Define variables
--- README.md
+++ README.md
@@ -6,16 +6,27 @@ The code is tested unfer `CMSSW_12_6_0` with `ROOT 6.24` and `Python 3.9.14`.
 Make sure `RooGaussStepBernstein` is properly installed! In my case, it is installed in the `combine`, specificly in `CMSSW_PATH/src/HiggsAnalysis/CombinedLimit/src/HZGRooPdfs.cxx`.
 
 # User Guide
-The minimization is highly integrated due to some problem with pyroot. It is defined in `Utilities/bkg_functions_fit.py`. The typical minimization of certain background pdf is like this:
+The functions are defined in `Utilities/bkg_functions_class.py`. A typical definition of a model is like this: 
 ```
-#PDF#Minimization(x = fit axis, gauss_mu = convolution gaussian mu, histogram, method, error = Poisson or SumW2, cat = categoty,
-                  ...PDF specific arguments...,
-                  printLevel = fitter print level, eps = EDM, offSet = offset of test statistics, strategy = please use 0)
+model = #PDF#Class(x, gauss_mu, cat, sigma_init, step_init, ...#PDF# specific arguments...)
 ```
-Some default values are assigned already, but feel free to play around with them.
+where `x` is the fitting axis, `gauss_mu` is the mean of the convolution Gaussian, `cat` is the category name, `sigma_init` is the initial value of Gaussian sigma, and `step_init` is the step position initial value. Some default values are assigned already, but feel free to play around with them.
+
+Then define a test statistics (either `RooChi2Var` or `RooNLLVar`) using a `RooDataHist` and `model.pdf`. The typical minimization of certain test statistics is like this:
+```
+Minimizer_#STAT#(STAT, printLevel, eps, offset, strategy)
+```
 
 ## F Test
-In the `F_test.py`, please change the sample directory. F test of multiple categories can happen in one run, and the print out will become messy. So please modify the file so that you do one category at a time. When calling the F test, please specify the fitting method (Chi2 or NLL).
+In the `F_test.py`, please change the sample directory. F test of multiple categories can happen in one run, and the print out will become messy. So please modify the file so that you do one category at a time. 
+
+One single F test is defined as
+```
+singleBernFTest(x, gauss_mu, histogram, cat, method, e_type, eps, offset, strategy)
+```
+where `x` is the fitting axis, `gauss_mu` is the mean of the convolution Gaussian, `histogram` is the `RooDataHist` to be fit, `cat` is the category name, `e_type` is the error type, `eps` is the EDM, `offset` controls whether using the offset, and `strategy` should be 0.
+
+When calling the F test, please specify the fitting method (Chi2 or NLL).
 ```
 bash$ python3 F_test.py Chi2
 ```
