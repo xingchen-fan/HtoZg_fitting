aef0319981d15b46e4230af501bc12874dd3ee66
xingchen-fan <xf82@cornell.edu>
2024-04-08 13:27:40 +0200

    update

--- Utilities/bias_class.py
+++ Utilities/bias_class.py
@@ -8,7 +8,7 @@ class BiasClass:
         else:
             self.nll = ROOT.RooNLLVar("nll_"+pdf.GetName(), "nll_"+pdf.GetName(), pdf, hist)
         self.corrNLL = 0.
-    def minimize(self, printLevel = -1, eps = 0.1, offSet = True):
+    def minimize(self, printLevel = -1, eps = 0.01, offSet = True):
         Minimizer_NLL(self.nll, -1, 100, False, 0)
         r = Minimizer_NLL(self.nll, printLevel, eps, offSet, 0)
         #print("Cov q = ", r.covQual(), " status = ", r.status(), end="\n")
