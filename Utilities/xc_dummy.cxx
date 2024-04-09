5705afed76cde6a8427756e3c26fbe95f3a40556
xingchen-fan <xf82@cornell.edu>
2024-04-10 00:33:10 +0200

    xc

---
+++ Utilities/xc_dummy.cxx
@@ -0,0 +1,5 @@
+#include "xc_dummy.h"
+
+dummy::dummy(float k){
+    std::cout << "test = "<<k << std::endl;
+}
---
+++ Utilities/xc_dummy.h
@@ -0,0 +1,7 @@
+#ifndef XC_DUMMY
+#define XC_DUMMY
+class dummy{
+    public:
+        dummy(float k);
+};
+#endif
