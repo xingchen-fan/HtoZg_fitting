61f78ad662290036619b8a932b1b7355f0fa1693
xingchen-fan <xf82@cornell.edu>
2024-04-12 12:49:22 +0200

    wsp

--- Make_combine_workspaces/make_worksapce_bias_test.py
+++ Make_combine_workspaces/make_worksapce_bias_test.py
@@ -42,47 +42,43 @@ mu_gauss = ROOT.RooRealVar("mu_gauss","always 0"       ,0.)
 MH = ROOT.RooRealVar("MH","MH"       ,124.7, 120., 130.)
 list = [x, y, w, bdt, year, lep, ph_eta, nlep, njet]
 
-# Cornell MC sample dat reader and make RooDataHist
+# Cornell MC and data sample dat reader and make RooDataHist
 x.setBins(260)
-reader = readDat(list, "../../sample/")
+reader = readDat(list, dir="../../sample/")
 reader.numCheck()
 cat = "u3"
 sig_hist = reader.data_hist_untagged3_sig
-bkg_hist = reader.data_hist_untagged3_bkg
-tot_hist = reader.data_hist_untagged3
+bkg_hist = reader.data_u3
+N = bkg_hist.sumEntries()
+N_sig = sig_hist.sumEntries()
 
-# Assume we have Bern2, Bern3, Pow1, Exp1, Exp2, Lau1, Lau2 in the profile, the signal model is DSCB
+print("N sig = ", N_sig)
+
+# Assume we have ...... in the profile, the signal model is DSCB
 bern2 = Bern2Class(x, mu_gauss, cat)
-bern3 = Bern2Class(x, mu_gauss, cat)
 pow1 = Pow1Class(x, mu_gauss, cat)
 exp1 = Exp1Class(x, mu_gauss, cat)
 exp2 = Exp2Class(x, mu_gauss, cat)
-lau1 = Lau1Class(x, mu_gauss, cat, p1 = -6, p2 = -5)
 lau2 = Lau2Class(x, mu_gauss, cat, p1 = -6, p2 = -5, p3 = -4)
 modg = ModGausClass(x,cat, lowx, lowx+65)
 
 sig_model = DSCB_Class(x, MH, cat)
 
-x.setBins(260)
-#reader = readDat(list, "/afs/cern.ch/user/f/fanx/public/samples/")
-reader = readDat(list, dir="../../sample/")
-reader.numCheck()
-
 x.setRange("signal",115, 132)
 sig_model.pdf.fitTo(sig_hist, ROOT.RooFit.SumW2Error(True),  ROOT.RooFit.Save(True),ROOT.RooFit.PrintLevel(-1),ROOT.RooFit.Strategy(0), ROOT.RooFit.Range("signal"))
 sig_model.setConst(True)
 
-profile = [bern2, bern3, pow1, exp1, exp2, lau1, lau2, modg]
+profile = [bern2, pow1, exp1, lau2, modg]
 stat_list = []
 error = ROOT.RooFit.DataError(ROOT.RooAbsData.Poisson)
 for model in profile:
-    stat_list.append(ROOT.RooChi2Var("stat_"+model.pdf.GetName(), "stat_"+model.pdf.GetName(), model.pdf,  bkg_hist, error))
+    stat_list.append(ROOT.RooNLLVar("stat_"+model.pdf.GetName(), "stat_"+model.pdf.GetName(), model.pdf,  bkg_hist))
 
 eps = 0.1
 strategy = 0
 for stat in stat_list:
     Minimizer_Chi2(stat, -1, 100, False, strategy)
-    r = Minimizer_Chi2(stat, -1, eps, False, strategy)
+    r = Minimizer_Chi2(stat, -1, eps, True, strategy)
     if LOG: r.Print("V")
     else: print(stat.GetName(), ": Cov q = ", r.covQual(), " status = ", r.status())
 
@@ -107,13 +103,29 @@ multipdf = ROOT.RooMultiPdf("multipdf_"+cat, "MultiPdf for "+cat, cate, models)
 # Penalty term
 #multipdf.setCorrectionFactor(0.)
 
-norm = ROOT.RooRealVar("multipdf_"+ cat +"_norm", "Number of background events", tot_hist.sumEntries(), 0, 3*tot_hist.sumEntries())
-f_out2 = ROOT.TFile("workspaces/workspace_bkg_profile_" + cat + ".root", "RECREATE")
+norm = ROOT.RooRealVar("multipdf_"+ cat +"_norm", "Number of background events", N, 0, 3*N)
+f_out2 = ROOT.TFile("workspaces/workspace_bkg_profile_bias_" + cat + ".root", "RECREATE")
 w_bkg = ROOT.RooWorkspace("workspace_bkg","workspace_bkg")
-getattr(w_bkg, "import")(tot_hist)
 getattr(w_bkg, "import")(cate)
 getattr(w_bkg, "import")(norm)
 getattr(w_bkg, "import")(multipdf)
 w_bkg.Print()
 w_bkg.Write()
 f_out2.Close()
+
+# Create toy histogram
+bias = False
+N_toy = 2
+if bias:
+    f_out3 = ROOT.TFile("workspaces/workspace_toy_" + cat + ".root", "RECREATE")
+    w_toy = ROOT.RooWorkspace("workspace_toy","workspace_toy")
+    for entry in profile:
+        x.setBins(260)
+        for i in range(N_toy):
+            hist_toy = entry.pdf.generateBinned(x, ROOT.RooFit.NumEvents(N))
+            hist_toy.SetNameTitle("hist_"+entry.name()+"_"+cat +"_"+ str(i), "hist_"+entry.name()+"_"+cat +"_"+ str(i))
+            getattr(w_toy, "import")(hist_toy)
+    w_toy.Write()
+    f_out3.Close()
+
+
